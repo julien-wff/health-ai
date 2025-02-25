5bb650289815a2fb56ebfa842d3a8dd9564ba9dc
Julien W <cefadrom1@gmail.com>
2025-02-25 23:19:54 +0100

    feat: automatic scroll to end of message view

--- components/chat/ChatMessages.tsx
+++ components/chat/ChatMessages.tsx
@@ -1,4 +1,5 @@
 import { UIMessage } from 'ai';
+import { useEffect, useRef } from 'react';
 import { ScrollView, Text, View } from 'react-native';
 
 interface ChatMessagesProps {
@@ -6,8 +7,18 @@ interface ChatMessagesProps {
 }
 
 export default function ChatMessages({ messages }: ChatMessagesProps) {
+    const scrollView = useRef<ScrollView>(null);
+
+    useEffect(() => {
+        updateScroll(true);
+    }, [ messages ]);
+
+    function updateScroll(animated = false) {
+        scrollView.current?.scrollToEnd({ animated });
+    }
+
     return (
-        <ScrollView className="flex-1 px-4">
+        <ScrollView className="flex-1 px-4" ref={scrollView} onLayout={() => updateScroll()}>
             {messages.map(m => (
                 <View key={m.id} className="my-2">
                     <Text className="font-bold text-slate-900 dark:text-slate-100">{m.role}</Text>
