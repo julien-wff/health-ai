9b6377a652309df4a780ebe84b4b2dfa3a3dae70
Julien W <cefadrom1@gmail.com>
2025-03-14 00:15:54 +0100

    feat: Android troubleshoot explanations

--- app/troubleshoot/android.tsx
+++ app/troubleshoot/android.tsx
@@ -1,5 +1,51 @@
-import { Text } from 'react-native';
+import { Text, useColorScheme, View } from 'react-native';
+import TroubleshootLayout from '@/components/troubleshoot/TroubleshootLayout';
+import { useHealthData } from '@/hooks/useHealthData';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'expo-router';
+import { Image } from 'expo-image';
+import TroubleshootingStepTitle from '@/components/troubleshoot/TroubleshootingStepTitle';
 
 export default function TroubleshootAndroid() {
-    return <Text>Troubleshoot Android</Text>;
+    const { empty } = useHealthData();
+    const router = useRouter();
+    const [ imageHeight, setImageHeight ] = useState(0);
+    const colorScheme = useColorScheme();
+
+    useEffect(() => {
+        if (!empty) router.back();
+    }, [ empty ]);
+
+    return <TroubleshootLayout>
+        <View className="flex gap-2">
+            <Text className="text-slate-800 dark:text-slate-200">
+                <Text className="font-bold">Health Connect{' '}</Text>
+                is the mandatory platform for
+                <Text className="font-bold">{' '}Health AI{' '}</Text>
+                to read your sleep, exercise, and steps provided by other apps.
+            </Text>
+
+            <Text className="text-slate-800 dark:text-slate-200">
+                However, it doesn't contain any recent data. This means that the provider applications have not written
+                their records to it.
+            </Text>
+        </View>
+
+        <Image onLayout={ev => setImageHeight(ev.nativeEvent.layout.width * (282 / 600))}
+               source={colorScheme === 'light'
+                   ? require('@/assets/troubleshooting/health-connect-schema-light.png')
+                   : require('@/assets/troubleshooting/health-connect-schema-dark.png')}
+               contentFit="contain"
+               style={{ width: '100%', height: imageHeight }}
+               transition={300}
+        />
+
+        <Text className="text-slate-800 dark:text-slate-200">
+            Please follow these instructions to check the synchronization:
+        </Text>
+
+        <TroubleshootingStepTitle index={1} title="Verify the Health Connect integration"/>
+        <TroubleshootingStepTitle index={2} title="Verify recent access on Health Connect"/>
+        <TroubleshootingStepTitle index={3} title="Look for data on Health Connect"/>
+    </TroubleshootLayout>;
 }
---
+++ components/troubleshoot/TroubleshootLayout.tsx
@@ -0,0 +1,31 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+import type { ReactNode } from 'react';
+import { Platform, Pressable, Text, View } from 'react-native';
+import { ArrowLeft } from 'lucide-react-native';
+import { useRouter } from 'expo-router';
+import { useColors } from '@/hooks/useColors';
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+export default function TroubleshootLayout({ children }: LayoutProps) {
+    const router = useRouter();
+    const colors = useColors();
+
+    return <SafeAreaView className="p-4 flex gap-4">
+        <View className="flex items-center gap-2 flex-row">
+            <Pressable className="p-2" onPress={() => router.back()}>
+                <ArrowLeft size={24} color={colors.text}/>
+            </Pressable>
+            <Text className="text-xl font-bold text-slate-900 dark:text-slate-50">
+                {Platform.select({
+                    ios: 'HealthKit Troubleshooting',
+                    android: 'Health Connect Troubleshooting',
+                })}
+            </Text>
+        </View>
+
+        {children}
+    </SafeAreaView>;
+}
