302513d1b60defddab7176458ba611e4599b30c3
Julien W <cefadrom1@gmail.com>
2025-04-01 10:12:14 +0200

    feat: merge chat endpoints to dynamically inject agent mode

---
+++ app/api/chat+api.ts
@@ -0,0 +1,37 @@
+import { getSystemPrompt, tools } from '@/utils/ai';
+import { google } from '@ai-sdk/google';
+import { type CoreMessage, streamText } from 'ai';
+import type { ChatRequestBody } from '@/utils/chat';
+
+
+interface RequestJson {
+    messages: CoreMessage[];
+    requestBody: ChatRequestBody;
+}
+
+export async function POST(req: Request) {
+    const { messages, requestBody } = await req.json() as RequestJson;
+    const { agentMode } = requestBody;
+
+    let systemPrompt: string;
+    if (agentMode === 'introvert') {
+        systemPrompt = getSystemPrompt({
+            tone: 'neutral and objective',
+            adviceMode: 'only when the user asks for it',
+        });
+    } else {
+        systemPrompt = getSystemPrompt({
+            tone: 'friendly and encouraging',
+            adviceMode: 'even if the user is not asking for it, be proactive in giving advice and suggestions',
+        });
+    }
+
+    const result = streamText({
+        model: google('gemini-2.0-flash-001'),
+        messages,
+        system: systemPrompt,
+        tools,
+    });
+
+    return result.toDataStreamResponse();
+}
